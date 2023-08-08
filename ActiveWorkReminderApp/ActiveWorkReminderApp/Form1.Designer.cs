85b4814a94fb98bb2afcb1f8429bc66fe4f2a211
shrawan <shrawankick@gmail.com>
2023-08-08 20:33:18 +0530

    Created ActiveWorkReminderApp for the user if we click on Start the program Start Notification

---
+++ ActiveWorkReminderApp/ActiveWorkReminderApp/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ActiveWorkReminderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.lblReminder = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(194, 239);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(198, 51);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // lblReminder
+            // 
+            this.lblReminder.AutoSize = true;
+            this.lblReminder.Location = new System.Drawing.Point(190, 179);
+            this.lblReminder.Name = "lblReminder";
+            this.lblReminder.Size = new System.Drawing.Size(202, 20);
+            this.lblReminder.TabIndex = 1;
+            this.lblReminder.Text = "Take a break and stand up!";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblReminder);
+            this.Controls.Add(this.btnStartStop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Label lblReminder;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ActiveWorkReminderApp/ActiveWorkReminderApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActiveWorkReminderApp
+{
+    public partial class Form1 : Form
+    {
+        private Timer reminderTimer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+
+            reminderTimer.Interval = 30000; // 1 hour in milliseconds
+            reminderTimer.Tick += ReminderTimer_Tick;
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (reminderTimer.Enabled)
+            {
+                reminderTimer.Stop();
+                btnStartStop.Text = "Start";
+            }
+            else
+            {
+                reminderTimer.Start();
+                btnStartStop.Text = "Stop";
+            }
+        }
+        private void ReminderTimer_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Time to take a 2-minute break! Stand up and look around.");
+        }
+    }
+}
